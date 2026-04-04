748c0131f95e1a33e2b01f3cb8da768a751d3c68
Alex-Windows <alexlopez.curru@gmail.com>
2026-04-03 19:36:08 -0600

    Sitio web - pagina inicial y configuraciones iniciales

---
+++ script.js
@@ -0,0 +1,202 @@
+"use strict";
+//========================================================
+//Definiendo Objetos del DOM
+const contenedorProductosEnOferta = document.querySelector(
+  "#productosContenedor",
+);
+//========================================================
+//definiendo arrays de objetos
+const descuento = 15;
+const productosConDescuento = [];
+const productosSinDescuento = [];
+const productos = [
+  {
+    id: 1,
+    nombre: "Celular marca patito",
+    descripcion: "Telefono celular 8GB ram 1TB almacenamiento",
+    precio: 1000,
+    oferta: false,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/pexels-igchandankumar-16442035.jpg",
+    categoria: "prueba",
+  },
+  {
+    id: 2,
+    nombre: "Headsets",
+    descripcion: "Headsets diseñados para la mayor comodidad",
+    precio: 500,
+    oferta: true,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/productos/Headsets.jpg",
+    categoria: "prueba",
+  },
+  {
+    id: 3,
+    nombre: "Audifonos inalambricos",
+    descripcion: "Audifonos inalambricos color blanco",
+    precio: 800,
+    oferta: true,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/productos/AudifonosInalambricos.jpg",
+    categoria: "prueba",
+  },
+  {
+    id: 4,
+    nombre: "Combo teclado y mouse",
+    descripcion: "Combo de teclado y mouse",
+    precio: 1000,
+    oferta: true,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/productos/tecladoMouse.jpg",
+    categoria: "prueba",
+  },
+  {
+    id: 5,
+    nombre: "USB Hub",
+    descripcion: "Hub USB con diferentes entradas",
+    precio: 250,
+    oferta: false,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/productos/usbHub.jpg",
+    categoria: "prueba",
+  },
+  {
+    id: 6,
+    nombre: "Adaptador tipo C",
+    descripcion: "Adaptador tipo C a usb",
+    precio: 100,
+    oferta: false,
+    stock: 100,
+    descuento: descuento / 100,
+    activo: function () {
+      if (this.stock > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    imagen: "./img/productos/adaptadorUSB.jpg",
+    categoria: "prueba",
+  },
+];
+
+//========================================================
+//Funcion para agregar los productos al array de productos en oferta y los que no estan en oferta
+function productosEnDescuento(array) {
+  const values = Object.values(array);
+  for (const {
+    id,
+    nombre,
+    descripcion,
+    precio,
+    imagen,
+    descuento,
+    activo,
+    oferta,
+  } of values) {
+    if (oferta) {
+      productosConDescuento.push({
+        nombre: nombre,
+        id: id,
+        descripcion: descripcion,
+        precioAnterior: precio,
+        activo: activo,
+        precio: precio - precio * descuento,
+        imagen: imagen,
+      });
+    } else if (!oferta) {
+      productosSinDescuento.push({
+        nombre: nombre,
+        id: id,
+        descripcion: descripcion,
+        activo: activo,
+        precio: precio,
+        imagen: imagen,
+      });
+    }
+  }
+}
+productosEnDescuento(productos);
+
+///========================================================
+//Destrucurando el array de objetos con descuentos para mostarlo en el array
+function mostrandoObjetosConDescuentosEnDOM(array) {
+  let tarjetaOfertas;
+  const entries = Object.entries(array);
+  contenedorProductosEnOferta.innerHTML = "";
+  for (let [
+    key,
+    { id, nombre, descripcion, precio, imagen, activo, precioAnterior },
+  ] of entries) {
+    if (activo) {
+      tarjetaOfertas = `
+       <div class="productoTarjeta center">    
+                     
+                <img class="productoImagen" src="${imagen}" alt="imagenDeProducto">
+           
+                <div class="productoInformacion">
+                  <div class="productoAlertaOferta center">
+                    <div class="circuloOferta"></div>
+                    <p>Producto en Oferta</p>       
+                  </div>
+                  <H1 class="productoNombre">${nombre}</H1>
+                  <p class="productoDescripcion">${descripcion}</p>
+                  <p class = "precios">Precio anterior Q.<span class="precioAnterior">${precioAnterior}</span></p>
+                  <b class = "precios"><p>Precio con descuento: Q.<span class="productoPrecio">${precio}</span></p></b>
+                </div>
+                <div class="productoBotones">
+                    <button class="comprar" id="${id}">Comprar!</button>
+                    <button class="productoMasInformacion">Ver detalles!</button>
+                </div>
+            </div>
+      `;
+    }
+    //
+    contenedorProductosEnOferta.insertAdjacentHTML(
+      "afterbegin",
+      tarjetaOfertas,
+    );
+  }
+}
+
+mostrandoObjetosConDescuentosEnDOM(productosConDescuento);
